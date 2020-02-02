3deec34afbfb86243ad1eae96f000102a91acc82
madhank93 <maddymadhan946@gmail.com>
2020-02-02 21:49:57 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PersonalExpense());
+
+class PersonalExpense extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Expense calculator'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Card(
+                child: Text('Card 1'),
+              ),
+            ),
+        Card(
+          child: Text('cARD 2'),
+        ),
+          ],
+        ),
+      ),
+    );
+  }
+}
