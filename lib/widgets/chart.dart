9b7ef484fc0b9ef7f61810638319363a609dd008
madhank93 <maddymadhan946@gmail.com>
2020-02-26 22:53:02 +0530

    Grouping all transaction and finding amount added for a day

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_personal_expense_app/models/transcation.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
 
-  
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekDay.day &&
+            recentTransaction[i].date.month == weekDay.month &&
+            recentTransaction[i].date.year == weekDay.year) {
+          totalSum = totalSum + recentTransaction[i].amount;
+        }
+      }
+      return {'day': DateFormat.E().format(weekDay), 'amount': 0.00};
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +30,7 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[
-
-        ],
+        children: <Widget>[],
       ),
     );
   }
