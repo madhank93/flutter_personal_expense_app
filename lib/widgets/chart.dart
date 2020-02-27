a52911582df215241c7e711bffbcce0526316368
madhank93 <maddymadhan946@gmail.com>
2020-02-27 22:04:38 +0530

    displaying the value

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -20,7 +20,7 @@ class Chart extends StatelessWidget {
           totalSum = totalSum + recentTransaction[i].amount;
         }
       }
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {'day': DateFormat.E().format(weekDay).substring(0,1), 'amount': totalSum};
     });
   }
 
@@ -31,7 +31,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactions.map((data) {
+          return Text('${data['day']} : ${data['amount']}');
+        }).toList(),
       ),
     );
   }
